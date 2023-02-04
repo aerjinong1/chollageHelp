c75522e5323fde0bcfefa0aef28682a495608ef0
wxz <wxz232246380@163.com>
2023-02-04 23:33:37 +0800

    add listcontroller

---
+++ src/main/java/com/collage/wxz/controller/ListController.java
@@ -0,0 +1,15 @@
+package com.collage.wxz.controller;
+import com.collage.wxz.util.JsonResult;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+@RestController
+@RequestMapping("list")
+public class ListController extends BaseController {
+
+}
